8958b347c6a7ee541171565b583956a134624550
Spongy11 <83553928+Spongy11@users.noreply.github.com>
2024-04-28 19:50:39 +0200

    Add files via upload

--- html/javascript/script.js
+++ html/javascript/script.js
@@ -1,1 +1,18 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const tabButtons = document.querySelectorAll('.tab-button');
+    const tabPanels = document.querySelectorAll('.tab-panel');
+  
+    tabButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        const targetTab = button.dataset.tab;
+        tabButtons.forEach(function(button) {
+          button.classList.remove('active');
+        });
+        tabPanels.forEach(function(panel) {
+          panel.classList.remove('active');
+        });
+        button.classList.add('active');
+        document.getElementById(targetTab).classList.add('active');
+      });
+    });
+  });
